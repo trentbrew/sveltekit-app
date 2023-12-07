acde25f94c475195f442bfdfc7db87cada2ab6d0
Trent Brew <tbrew1023@gmail.com>
2023-12-06 22:15:29 -0800

    tailwind and daisy

---
+++ src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<main class="bg-black text-[#ffffffaa] h-screen w-screen flex justify-center items-center">
+  <h1>Welcome to SvelteKit</h1>
+  <p>Visit <a class="underline hover:text-gray-600" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</main>
